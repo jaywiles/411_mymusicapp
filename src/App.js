32845c27bb2023df23ce163d986304eaca5cf82a
jaywiles <jay.wiles@gmail.com>
2019-11-07 20:40:39 -0600

    beginnings of signin

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React, {Component} from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import NavBar from './components/NavBar';
-// import {Button, TextField} from '@material-ui/core';
+import SignIn from './components/SignIn'
+import {Button, TextField} from '@material-ui/core';
 
 class App extends Component {
 	
-  	constructor(props) {
-		super(props);
-		this.state = {
-			loggedIn: false,
-		}
-  	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    }
+  }
 
-  	render() { return (
+  render() { return (
 		<div id="container">
 			<header>
 				<NavBar />
 			</header>
+
+      <section className="login-class">
+        <SignIn />
+      </section>
 
 			<section className="">
 				<Dashboard />
