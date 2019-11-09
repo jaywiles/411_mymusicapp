e83c6998dc9a0fd029226c78ce8e171c065151ab
jaywiles <jay.wiles@gmail.com>
2019-11-09 12:29:00 -0600

    changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Online from './Online';
 import {FormGroup, FormControlLabel, Switch} from '@material-ui/core';
 
 class Dashboard extends Component {
@@ -9,36 +10,16 @@ class Dashboard extends Component {
       someString: '',
       notifications: [],
       online: true,
+      testing: 'this sucks',
 		};
   }
   
-  onlineChange = () => {
-    let status = this.state.online === true ? false : true
-    this.setState({
-      online: status
-    })
-  }
-
-  offlineMessage = () => {
-    if (!this.state.online) {
-      return (
-        <h3>Your application is offline. You won't be able to share or stream music to other devices.</h3>
-      )
-    }
-  }
 
 	render() {
     return (
       <div className="dashboard">
         <div className="online-message">
-          <FormGroup>
-            <FormControlLabel
-              control={<Switch checked={this.state.online} onChange={this.onlineChange} />}
-              label="Online"
-            />
-            {console.log(this.state.online)}
-          </FormGroup>
-          {this.offlineMessage()}
+          <Online />
         </div>
         <p>Dashboard</p>
       </div>
