6249c6f7bb962ab1298e55840860acf7626afeba
jaywiles <jay.wiles@gmail.com>
2019-11-09 10:08:46 -0600

    something else

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ class App extends Component {
           <NavBar />
         </header>
 
-        {/* <section className="">
+        <section className="dashboard-container">
           <Dashboard />
-        </section> */}
+        </section>
       </div>
       )
     }
