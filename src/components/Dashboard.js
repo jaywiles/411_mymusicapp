924820db53cd72330454f69889fad0c5b2ee4ddf
jaywiles <jay.wiles@gmail.com>
2019-11-09 14:06:26 -0600

    working

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import Online from './Online';
-// import {FormGroup, FormControlLabel, Switch} from '@material-ui/core';
+// import Online from './Online';
+import {FormGroup, FormControlLabel, Switch} from '@material-ui/core';
 
 class Dashboard extends Component {
 
@@ -13,15 +13,46 @@ class Dashboard extends Component {
       testing: 'this sucks',
 		};
   }
+
+  onlineChange = (notification) => {
+    let status = this.state.online === true ? false : true
+    let newNotification = [...this.state.notifications, notification]
+    console.log(notification)
+    this.setState({
+      online: status
+    })
+    if (this.state.online === false) {
+      this.setState({
+        notifications: newNotification
+        // console.log("Your application is offline. You won't be able to share or stream music to other devices.")
+      })
+    }
+  }
+
   
 
 	render() {
     return (
       <div className="dashboard">
         <div className="online-message">
-          <Online />
+          <FormGroup>
+            {console.log(this.state.online)}
+            <FormControlLabel
+              control={<Switch checked={this.state.online} onChange={this.onlineChange} />}
+              label="Online"
+            />
+            {/* <h3>Test: </h3>
+              {this.state.notifications.map(note =>{
+                console.log(note)
+              return (
+                <p>{note}</p>
+              )
+            })} */}
+          </FormGroup>
+          <p>{this.state.notifications}</p>
         </div>
         <p>Dashboard</p>
+        
       </div>
     )
   }
--- src/components/Online.js
+++ src/components/Online.js
@@ -1,59 +1,59 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 // import Dashboard from './Dashboard'
-import {FormGroup, FormControlLabel, Switch} from '@material-ui/core';
+// import {FormGroup, FormControlLabel, Switch} from '@material-ui/core';
 
-class Online extends Component {
+// class Online extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-      someString: '',
-      // notifications: [],
-      // online: true,
-		};
-  }
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+//       someString: '',
+//       // notifications: [],
+//       // online: true,
+// 		};
+//   }
   
-  // onlineChange = () => {
-  //   let status = this.state.online === true ? false : true
-  //   this.setState({
-  //     online: status
-  //   })
-  // }
+//   // onlineChange = () => {
+//   //   let status = this.state.online === true ? false : true
+//   //   this.setState({
+//   //     online: status
+//   //   })
+//   // }
 
-  // offlineMessage = () => {
-  //   if (!this.state.online) {
-  //     return (
-  //       <h3>Your application is offline. You won't be able to share or stream music to other devices.</h3>
-  //     )
-  //   }
-  // }
+//   // offlineMessage = () => {
+//   //   if (!this.state.online) {
+//   //     return (
+//   //       <h3>Your application is offline. You won't be able to share or stream music to other devices.</h3>
+//   //     )
+//   //   }
+//   // }
 
-  onlineChange = () => {
-    let status = this.online === true ? false : true
-    this.setState({
-      online: status
-    })
-    if (this.online === false) {
-      console.log('this works')
-      this.notifications.bind("Your application is offline. You won't be able to share or stream music to other devices.")
-    }
-  }
+//   onlineChange = () => {
+//     let status = this.props.online === true ? false : true
+//     this.setState({
+//       online: status
+//     })
+//     if (this.props.online === false) {
+//       console.log('this works')
+//       this.props.notifications.bind("Your application is offline. You won't be able to share or stream music to other devices.")
+//     }
+//   }
 
-	render() {
-    return (
-      <div className="online-container">
-        <FormGroup>
-          <FormControlLabel
-            control={<Switch checked={this.status} onChange={this.onlineChange} />}
-            label="Online"
-          />
-          {/* <h3>Test: {this.state.testing}</h3> */}
-        </FormGroup>
-        {/* {this.offlineMessage()} */}
-      </div>
-    )
-  }
+// 	render() {
+//     return (
+//       <div className="online-container">
+//         <FormGroup>
+//           <FormControlLabel
+//             control={<Switch checked={this.status} onChange={this.onlineChange} />}
+//             label="Online"
+//           />
+//           {/* <h3>Test: {this.state.testing}</h3> */}
+//         </FormGroup>
+//         {/* {this.offlineMessage()} */}
+//       </div>
+//     )
+//   }
   
-}
+// }
 
-export default Online;
+// export default Online;
