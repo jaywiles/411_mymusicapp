f0dc36f32a3fdfc5be2555b5ea61684a34bf1be7
State Tyler <statetyler@State-Tylers-MacBook-Pro.local>
2019-11-12 19:59:02 -0600

    fixed App and Dashboard

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import SignIn from './components/SignIn';
+import Dashboard from './components/Dashboard';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loggedIn: false,
+		}
+	}
+
+	toggleSignIn = () => {
+		this.setState({ loggedIn: !this.state.loggedIn })
+	}
+
+	render() {
+		return (
+			<div id="container">
+				<header>
+					<NavBar />
+				</header>
+
+				<section className="dashboard trim gutter">
+					{ this.state.loggedIn && (
+						<Dashboard />
+					)}
+					{ !this.state.loggedIn && (
+						<SignIn signIn={this.toggleSignIn} />
+					)}
+				</section>
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+import OnlineSwitch from './OnlineSwitch';
+import VolumeSlider from './VolumeSlider';
+import SoundQuality from './SoundQuality';
+
+class Dashboard extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			notifications: [],
+			online: true,
+			volume: 10,
+			quality: 'normal',
+		};
+	}
+
+	// this recives a string and adds or removes it based on if it is in the notifications array
+	toggleNotification = ( msg ) => {
+		const msgIndex = this.state.notifications.indexOf( msg )
+
+		if ( this.state.notifications.includes( msg ) ) {
+			this.state.notifications.splice( msgIndex, 1 )
+		} else {
+			this.setState({ notifications: [...this.state.notifications, msg] })
+		}
+	}
+	addNotification = ( msg ) => {
+		this.setState({ notifications: [...this.state.notifications, msg] })
+	}
+	removeNotification = ( msg ) => {
+		const msgIndex = this.state.notifications.indexOf( msg )
+		this.state.notifications.splice( msgIndex, 1 )
+	}
+
+	toggleOnline = () => {
+		this.setState({ online: !this.state.online })
+
+		const msg = "Your application is offline. You won't be able to share or stream music to other devices."
+
+		this.toggleNotification( msg )
+	}
+	
+	volumeSlider = () => {
+
+		// Slider stuff
+
+	}
+
+	soundQuality = ( event ) => {
+		this.setState({ quality: event.target.value })
+
+		const msg = "Music quality is degraded. Increase quality if your connection allows it."
+
+		if ( (event.target.value === 'low') && !this.state.notifications.includes( msg ) ) {
+			this.addNotification( msg )
+		}
+		if ( (event.target.value !== 'low') && this.state.notifications.includes( msg ) ) {
+			this.removeNotification( msg )
+		}
+	}
+
+	render() {
+		return (
+			<div className="dashboard wrapper">
+
+				<div className="dashboard-control-row">
+					
+					<div class="dashboard-control-col">
+						<OnlineSwitch
+							onlineState={this.state.online}
+							onlineFunc={this.toggleOnline}>
+						</OnlineSwitch>
+					</div>
+
+					<div class="dashboard-control-col">
+						<VolumeSlider
+							volumeState={this.state.volume}
+							volumeFunc={this.volumeSlider}>
+						</VolumeSlider>
+					</div>
+
+					<div class="dashboard-control-col">
+						<SoundQuality 
+							qualityState={this.state.quality} 
+							qualityFunc={this.soundQuality}>	
+						</SoundQuality>
+					</div>
+
+				</div> {/* END dashboard-control-row */}
+
+				<div className="notifications frame-top">
+					<h3>Notifications:</h3>
+					{this.state.notifications.map((item, index) => (
+						<p key={index}>{item}</p>
+					))}
+				</div>
+
+			</div>
+		)
+	}
+	
+}
+
+export default Dashboard;
